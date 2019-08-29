8da5283a0ad934e36d60e3bd93e11faa71d398c3
Kleyner <kleineramariz4@gmail.com>
2019-08-29 08:29:36 -0500

    Iniciando proyecto medicalSoft

---
+++ src/medicalsoft/Persona.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package medicalsoft;
+
+/**
+ *
+ * @author estudiante
+ */
+public class Persona {
+    
+    public Persona(String nom, String ape) {
+        this.nombres = nom;
+        this.apellidos = ape;
+    }
+    
+    private String nombres;
+    private String apellidos;
+    private String fecha_nacimiento;
+    private String identificacion;
+    private String genero;
+    private String direccion;
+    private String email;
+    private String telefono;
+    private String celular;
+
+    public String getNombres() {
+        return nombres.toUpperCase();
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(String fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+    
+    
+}
